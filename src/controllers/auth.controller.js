a5c77524dbfb65ffc0c8e83fadf5f187313910a7
argun8394 <argundoganali@gmail.com>
2024-05-04 17:34:34 +0300

    jwt

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 "use strict"
 
+
+const jwt = require("jsonwebtoken")
+
 const Personnel = require("../models/personnel.model")
 
 module.exports = {
@@ -14,6 +17,35 @@ module.exports = {
 
                 if (user.isActive) {
                     //Login OK
+
+                    const accessData = {
+                        _id: user._id,
+                        departmentId: user.departmentId,
+                        firstName: user.firstName,
+                        lastName: user.lastName,
+                        isActive: user.isActive,
+                        isAdmin: user.isAdmin,
+                        isLead: user.isLead,
+                    }
+
+                    const accessToken = jwt.sign(accessData, process.env.ACCESS_KEY, { expiresIn: '30m' })
+
+                    const refreshData = {
+                        username: user.username,
+                        password: user.password,
+                    }
+
+                    const refreshToken = jwt.sign(refreshData, process.env.REFRESH_KEY, { expiresIn: '3d' })
+
+                    res.send({
+                        error: false,
+                        token: {
+                            accessToken,
+                            refreshToken
+                        }
+                    })
+
+
                 } else {
                     res.errorStatusCode = 401
                     throw new Error("This account is not active.")
