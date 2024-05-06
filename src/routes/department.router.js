12fefbfa622f3343cfa692793d22c90c95540865
argun8394 <argundoganali@gmail.com>
2024-05-06 18:20:04 +0300

    update_routers

--- src/routes/department.router.js
+++ src/routes/department.router.js
@@ -4,21 +4,22 @@ const router = require('express').Router()
 /* ------------------------------------------------------- */
 
 const department = require('../controllers/department.controller')
+const permissions = require('../middlewares/permissions')
 
 // URL: /departments
 
 router.route('/')
-    .get(department.list)
+    .get(permissions.isLogin, department.list)
     .post(department.create)
 
 router.route('/:id')
-    .get(department.read)
-    .put(department.update)
-    .patch(department.update)
-    .delete(department.delete)
+    .get(permissions.isAdminOrLead, department.read)
+    .put(permissions.isAdminOrLead, department.update)
+    .patch(permissions.isAdminOrLead, department.update)
+    .delete(permissions.isAdmin, department.delete)
 
 //Calling personnels from a specific department
-router.get('/:id/personnels', department.personnels) //http://127.0.0.1:8000/departments/6601812d8ae721b1a4efffb2/personnels
+router.get('/:id/personnels', permissions.isAdminOrLead, department.personnels) //http://127.0.0.1:8000/departments/6601812d8ae721b1a4efffb2/personnels
 
 /* ------------------------------------------------------- */
 module.exports = router
