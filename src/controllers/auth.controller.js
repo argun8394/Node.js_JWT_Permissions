13207e0a6fbf246c3091dea008782d58c75945b5
argun8394 <argundoganali@gmail.com>
2024-05-07 18:24:49 +0300

    auth_controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,12 +1,16 @@
 "use strict"
+/* --------------------------------------- */
+// JWT
+// npm i jsonwebtoken
 
-
-const jwt = require("jsonwebtoken")
-
-const Personnel = require("../models/personnel.model")
+const jwt = require('jsonwebtoken')
+const Personnel = require('../models/personnel.model')
+const checkUserAndSetToken = require('../helpers/checkUserAndSetToken')
 
 module.exports = {
+
     login: async (req, res) => {
+        /*
         const { username, password } = req.body
 
         if (username && password) {
@@ -16,7 +20,7 @@ module.exports = {
             if (user) {
 
                 if (user.isActive) {
-                    //Login OK
+                // Login OK
 
                     const accessData = {
                         _id: user._id,
@@ -27,14 +31,12 @@ module.exports = {
                         isAdmin: user.isAdmin,
                         isLead: user.isLead,
                     }
-
-                    const accessToken = jwt.sign(accessData, process.env.ACCESS_KEY, { expiresIn: '30m' })
+                    const accessToken = jwt.sign(accessData, process.env.ACCESS_KEY, { expiresIn: '10m' })
 
                     const refreshData = {
                         username: user.username,
-                        password: user.password,
+                        password: user.password
                     }
-
                     const refreshToken = jwt.sign(refreshData, process.env.REFRESH_KEY, { expiresIn: '3d' })
 
                     res.send({
@@ -45,23 +47,56 @@ module.exports = {
                         }
                     })
 
-
                 } else {
+
                     res.errorStatusCode = 401
-                    throw new Error("This account is not active.")
+                    throw new Error('This account is not active.')
                 }
             } else {
+
                 res.errorStatusCode = 401
-                throw new Error("Wrong username  or password.")
+                throw new Error('Wrong username or password.')
             }
         } else {
+
             res.errorStatusCode = 401
-            throw new Error("Please entry username and password.")
+            throw new Error('Please enter username and password.')
+        }
+        */
+        const checkUser = await checkUserAndSetToken(req.body)
+        if (checkUser.error) {
+            res.errorStatusCode = 401
+            throw new Error(checkUser.message)
+        } else {
+            res.send(checkUser)
         }
     },
 
     refresh: async (req, res) => {
+        const refreshToken = req.body?.token?.refresh || null
 
+        if (refreshToken) {
+
+            const jwtData = jwt.verify(refreshToken, process.env.REFRESH_KEY)
+
+            if (jwtData) {
+
+                const checkUser = await checkUserAndSetToken(jwtData, false)
+                if (checkUser.error) {
+                    res.errorStatusCode = 401
+                    throw new Error(checkUser.message)
+                } else {
+                    res.send(checkUser)
+                }
+
+            } else {
+                res.errorStatusCode = 401
+                throw new Error('Wroong JWT Token')
+            }
+        } else {
+            res.errorStatusCode = 401
+            throw new Error('Please entry token.refresh')
+        }
     },
 
     logout: async (req, res) => {
